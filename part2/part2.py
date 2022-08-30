a7360256a3719613c4bf050832d7ce22065501dc
prakul-27 <78408581+prakul-27@users.noreply.github.com>
2022-08-31 01:49:42 +0530

    enumerated batches in training data

--- part2/part2.py
+++ part2/part2.py
@@ -1,14 +1,15 @@
 import csv
 import numpy as np
-
-theta = np.array([3, 1, 2])
-x = np.zeros(len(theta),)
+import pandas as pd
 
 def h(theta, x):
     epsilon = np.random.normal(0, 2, 1)[0]
     return np.dot(theta, x) + epsilon
 
 def sample_data():
+    theta = np.array([3, 1, 2])
+    x = np.zeros(len(theta),)
+
     with open('data/train_data.csv', 'w', newline='') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=['X_1', 'X_2', 'Y'])
         csv_writer.writeheader()
@@ -26,4 +27,21 @@ def sample_data():
             csv_writer.writerow(row)
     return
 
-sample_data()
+def cost(theta, xs, ys): 
+    return (1/(2*len(xs))) * np.sum([np.power(y - h(theta, x), 2) for x, y in zip(xs, ys)])
+
+def stochastic_gradient_descent(r, eta=0.001):
+    theta = np.zeros(3,)
+    xs = pd.read_csv('data/train_data.csv').values.ravel()
+    ys = pd.read_csv('data/train_data.csv').values.ravel()
+    
+    index = 0
+    for epoch in range(len(xs)/r):        
+        last, x, y = index+r, [], []
+        while index < min(last, len(xs)):
+            x.append(xs[index])
+            y.append(ys[index])
+            index += 1
+        x, y = np.array(x), np.array(y)
+
+    return
