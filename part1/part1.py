459e337c92e6e199d196fd236ab86c38d199c313
prakul-27 <78408581+prakul-27@users.noreply.github.com>
2022-09-05 23:52:56 +0530

    sgd added

--- part1/part1.py
+++ part1/part1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import csv
 
 def read_data():
     xs = pd.read_csv('data/linearX.csv').values
@@ -19,33 +20,34 @@ def h(x, theta, bias): # hypothesis
 def cost(xs, ys, theta, bias): # J(theta)
     return (1/(2*len(xs))) * np.sum([np.power(y - h(x, theta, bias), 2) for x, y in zip(xs, ys)])
     
-def batch_grad_descent(xs, ys, theta, bias, eta = 0.01, epsilon = 0.01, iters = 100, which=0):
-    if which == 0:
-        n = 0
-        while True:
-            n += 1
-            new_theta = theta + eta * np.sum([(y - h(x, theta, bias)) * x for x, y in zip(xs, ys)])         
-            if abs(new_theta - theta) < epsilon:                
-                break
-            theta = new_theta
-            if n % 5 == 0:
-                print('theta =', end=' ')
-                print(theta, end=',')
-                print('cost =', end= ' ')
-                print(cost(xs, ys, theta, bias))
-    else:
-        for i in range(iters):
-            theta = theta + eta * np.sum([(y - h(x, theta, bias)) * x for x, y in zip(xs, ys)])
-            if i % 10 == 0:
-                print('theta = '+str(theta))
-                print('cost = '+str(cost(xs, ys, theta, bias)))
-                print()
-    return theta
+def batch_grad_descent(xs, ys, theta, bias, eta = 0.01, epsilon = 0.01, iters = 100):    
+    n = 0
+    costs, thetas, biases = [],[],[]    
+    while True:
+        costs.append(cost(xs, ys, theta, bias))
+        thetas.append(theta)
+        biases.append(bias)
+        n += 1
+        new_theta = theta + eta * np.sum([(y - h(x, theta, bias)) * x for x, y in zip(xs, ys)])
+        new_bias = bias + eta * np.sum([(y - h(x, theta, bias)) for x, y in zip(xs, ys)])                
+        if abs(new_theta - theta) < epsilon and abs(new_bias-bias) < epsilon:                
+            break
+        theta = new_theta
+        bias = new_bias
+        if n % 5 == 0:
+            print('theta =', end=' ')
+            print(theta, end=',')
+            print('cost =', end= ' ')
+            print(cost(xs, ys, theta, bias))    
+    with open('train_data.csv', w) as csv_file:
+        
+    return theta, bias
 
 if __name__ == '__main__':
     xs,ys = read_data()
     xs,ys = normalize_data(xs, ys)
-    theta, bias = 0.0, -0.01
-    theta = batch_grad_descent(xs, ys, theta, bias, iters = 1000, which=0, eta=0.005, epsilon=1/1e15)
+    theta, bias = 0.0, 0.0
+    theta, bias = batch_grad_descent(xs, ys, theta, bias, iters = 1000, which=0, eta=0.005, epsilon=1/1e15)
     print('final theta = '+str(theta))
+    print('final_bias = '+str(bias))
     print('final cost = '+str(cost(xs, ys, theta, bias)))
--- part2/part2.py
+++ part2/part2.py
@@ -1,10 +1,10 @@
 import csv
 import numpy as np
 import pandas as pd
+import random
+import pprint
+random.seed()
 
-def h(theta, x):
-    epsilon = np.random.normal(0, 2, 1)[0]
-    return np.dot(theta, x) + epsilon
 
 def sample_data():
     theta = np.array([3, 1, 2])
@@ -17,7 +17,8 @@ def sample_data():
         for _ in range(int(1e6)):            
             x1, x2 = np.random.normal(3, 4, 1)[0], np.random.normal(-1, 4, 1)[0]            
             x = np.array([1, x1, x2])
-            y = h(theta, x)
+            epsilon = np.random.normal(0, 2, 1)[0]
+            y = np.dot(theta, x) + epsilon
             
             row = {
                 'X_1': x1,
@@ -27,21 +28,86 @@ def sample_data():
             csv_writer.writerow(row)
     return
 
-def cost(theta, xs, ys): 
-    return (1/(2*len(xs))) * np.sum([np.power(y - h(theta, x), 2) for x, y in zip(xs, ys)])
+def cost(theta, x, y):
+    return 1/(2*len(x))*sum((y-np.matmul(x, theta))**2)
 
-def stochastic_gradient_descent(r, eta=0.001):
+def batches(x, r):
+	batch_list = []
+	current_batch = []
+	for xi in x:
+		current_batch.append(xi)				
+		if len(current_batch) == r:
+			batch_list.append(current_batch)
+			current_batch = []		
+	if current_batch:
+		batch_list.append(current_batch)
+	return batch_list
+
+def stochastic_gradient_descent(r, eta=0.001, epsilon=0.001, conv_iters=1000):
     theta = np.zeros(3,)
-    xs = pd.read_csv('data/train_data.csv').values.ravel()
-    ys = pd.read_csv('data/train_data.csv').values.ravel()
+    df = pd.read_csv('data/train_data.csv')
+    x1, x2, Y = df['X_1'].ravel(), df['X_2'].ravel(), df['Y'].ravel()
+    X = np.array([[1.0, x1i, x2i] for x1i, x2i in zip(x1, x2)])
     
-    index = 0
-    for epoch in range(len(xs)/r):        
-        last, x, y = index+r, [], []
-        while index < min(last, len(xs)):
-            x.append(xs[index])
-            y.append(ys[index])
-            index += 1
-        x, y = np.array(x), np.array(y)
+    X = np.array(batches(X, r))
+    Y = np.array(batches(Y, r))    
+    
+    total_iters = 0
+    has_converged = False
+    iters, cur_theta_sum, prev_avg_theta = 0, np.zeros(3,), np.zeros(3,)
+        		
+    
+    with open('r1000000.csv', 'w', newline='') as csv_file:        
+        csv_writer = csv.DictWriter(csv_file, fieldnames=['theta0', 'theta1', 'theta2', 'cost'])	    
+        csv_writer.writeheader()       
+        while not has_converged:                        
+            for x, y in zip(X, Y):
+                total_iters += 1
+                iters += 1
+                #update theta                         
+                theta = theta + eta*(1/len(x))*np.matmul(np.transpose(y-np.matmul(x, theta)), x)            
+                #print(theta)
+                cur_theta_sum += theta                 
+                csv_writer.writerow({"theta0":theta[0], "theta1":theta[1], "theta2":theta[2], "cost":cost(theta, x, y)})
+                if iters == conv_iters:                
+                    iters = 0                                
+                    cur_avg_theta = cur_theta_sum
+                    diff = pow(np.sum([(old-new)**2 for old, new in zip(prev_avg_theta, cur_avg_theta)]), 0.5)                 
+                    #print(diff)
+                    if diff < epsilon:
+                        has_converged = True
+                        break
+                    prev_avg_theta = cur_avg_theta
+                    cur_theta_sum = np.zeros(3,)                
+        return theta, total_iters
 
-    return
+print(stochastic_gradient_descent(1000000, epsilon = 0.00001, conv_iters=1))
+
+# models
+# r = 1, epsilon = 1, conv_iters=100, iters = 11700, conv_val = [2.90096727, 1.00398103, 1.9958781 ]
+# r = 100, epsilon = 0.1, conv_iters=50, iters = 7250, conv_val = [2.97415026, 1.00340122, 1.99621081]
+# r = 10000, epsilon = 0.001, conv_iters=10, iters = 8190, conv_val = [2.98299967, 1.00181044, 1.99969435]
+# r = 1000000, epsilon = 0.00001, conv_iters=1, iters = 8560, conv_val = [2.98714405, 1.00147602, 1.99980244] 
+
+#theta = np.array([2.98714405, 1.00147602, 1.99980244])
+def test(theta):
+    df = pd.read_csv('data/q2test.csv')
+    x1, x2, Y = df['X_1'].ravel(), df['X_2'].ravel(), df['Y'].ravel()
+    X = np.array([[1.0, x1i, x2i] for x1i, x2i in zip(x1, x2)])
+    return cost(theta, X, Y)
+
+# costs
+# r = 1, cost = 0.9895715244767835 
+# r = 100, cost = 0.9848810836329218 
+# r = 10000, cost = 0.9832557604388013
+# r = 1000000, cost = 0.9831320884069167
+# original hypothesis, cost = 0.9829469215000091
+
+def test_with_original():
+    theta = np.array([3, 1, 2])    
+    df = pd.read_csv('data/q2test.csv')
+    x1, x2, Y = df['X_1'].ravel(), df['X_2'].ravel(), df['Y'].ravel()
+    X = np.array([[1.0, x1i, x2i] for x1i, x2i in zip(x1, x2)])    
+    Y = df['Y'].ravel()
+    return cost(theta, X, Y)
+
